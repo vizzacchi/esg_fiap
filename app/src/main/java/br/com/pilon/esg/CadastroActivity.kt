81e166f6a556ca6c55a61acaea5f6c52b59e543a
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2023-03-03 20:44:15 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/pilon/esg/CadastroActivity.kt
@@ -0,0 +1,14 @@
+package br.com.pilon.esg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.pilon.esg.R
+
+
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
