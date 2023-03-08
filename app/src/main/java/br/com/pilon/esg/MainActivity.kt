e5e3dd4affa344fd7583a611ca1664eeacf3e30c
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2023-03-07 21:06:57 -0300

    Subindo Activity APP

--- app/src/main/java/br/com/pilon/esg/MainActivity.kt
+++ app/src/main/java/br/com/pilon/esg/MainActivity.kt
@@ -3,19 +3,28 @@ package br.com.pilon.esg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import br.com.pilon.esg.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val newUsuario = findViewById<TextView>(R.id.txvNovoUsuario)
+        val btnlogin = findViewById<Button>(R.id.btnLogin)
 
         newUsuario.setOnClickListener{
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
+        }
+
+        btnlogin.setOnClickListener {
+            val intent = Intent(this,appActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
---
+++ app/src/main/java/br/com/pilon/esg/appActivity.kt
@@ -0,0 +1,11 @@
+package br.com.pilon.esg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class appActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
