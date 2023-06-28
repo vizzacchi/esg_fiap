f0e96357f2658d5496b51b6a2e5b1c9d2648d150
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2023-06-28 20:25:05 -0300

    Subindo activy Voluntários

--- app/src/main/java/br/com/pilon/esg/appActivity.kt
+++ app/src/main/java/br/com/pilon/esg/appActivity.kt
@@ -1,11 +1,21 @@
 package br.com.pilon.esg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class appActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+
+        val btnVoluntario = findViewById<Button>(R.id.btnVoluntario)
+
+        btnVoluntario.setOnClickListener {
+            val intent = Intent(this,voluntary::class.java)
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/br/com/pilon/esg/voluntary.kt
@@ -0,0 +1,11 @@
+package br.com.pilon.esg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class voluntary : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voluntary)
+    }
+}
